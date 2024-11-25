703736cb8db1253b01951f18dcf6ad12da19a98e
Mohammad-Rishad-p <r25richu@gmail.com>
2024-11-25 09:45:58 +0530

    Update

---
+++ ExamApp/Preview Content/Fav.swift
@@ -0,0 +1,20 @@
+//
+//  Fav.swift
+//  ExamApp
+//
+//  Created by ddukk9 on 22/11/24.
+//
+
+import SwiftUI
+
+struct Fav: View {
+    var body: some View {
+        Text("Fav")
+    }
+}
+
+struct Fav_Previews: PreviewProvider {
+    static var previews: some View {
+        Fav()
+    }
+}
